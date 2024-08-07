802aadd157f9dc0b28d95c177eeb4093c86f57b5
Vajihe Ahmadi <vajiheahmadi78@gmail.com>
2024-08-07 16:47:05 +0330

    Feat:create Definition component with styles

--- components/templates/HomePage.js
+++ components/templates/HomePage.js
@@ -4,12 +4,14 @@ import styles from "./HomePage.module.css"
 //Components
 import Banner from '../modules/Banner'
 import Attributes from '../modules/Attributes'
+import Definition from '../modules/Definition'
 
 function HomePage() {
   return (
     <div className={styles.container}>
       <Banner/>
       <Attributes/>
+      <Definition/>
     </div>
   )
 }
