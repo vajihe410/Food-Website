6efe587bd924e4d45c07af300853d650382947d7
Vajihe Ahmadi <vajiheahmadi78@gmail.com>
2024-08-09 18:13:37 +0330

    Feat:use state for query and set query

--- components/templates/CategoriesPage.js
+++ components/templates/CategoriesPage.js
@@ -1,27 +1,41 @@
 import React, { useState } from 'react'
+import { useRouter } from 'next/router'
 //Styles
 import styles from "./CategoriesPage.module.css"
 
 function CategoriesPage() {
+    const router = useRouter()
+
+    const [query, setQuery] = useState({difficulity:"" , time:"" })
+
+    const changeHandler = (event) => {
+        setQuery({...query,[event.target.name] : event.target.value})
+    }
+
+    const searchHandler = () => {
+        router.push({
+            pathname:"/categories",
+            query
+        })
+    }
   return (
     <div className={styles.container}>
         <h2>Categories</h2>
         <div className={styles.subContainer}>
             <div className={styles.select}>
-                <select>
-                    <option>Difficulity</option>
-                    <option>Easy</option>
-                    <option>Medium</option>
-                    <option>Hard</option>
+                <select value={query.difficulity} name='difficulity'  onChange={changeHandler}>
+                    <option value="">Difficulity</option>
+                    <option value="Easy">Easy</option>
+                    <option value="Medium">Medium</option>
+                    <option value="Hard">Hard</option>
                 </select>
-                <select>
-                    <option>Cooking Time</option>
-                    <option>More than 30 min</option>
-                    <option>Less than 30 min</option>
+                <select value={query.time} name='time' onChange={changeHandler}>
+                    <option value="">Cooking Time</option>
+                    <option value="more">More than 30 min</option>
+                    <option value="less">Less than 30 min</option>
                 </select>
-                <button>Search</button>
+                <button onClick={searchHandler}>Search</button>
             </div>
-
         </div>
     </div>
   )
