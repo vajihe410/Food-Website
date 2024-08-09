93d32e56d9c53bb6c5d9004743c9708433b59d58
Vajihe Ahmadi <vajiheahmadi78@gmail.com>
2024-08-09 15:36:14 +0330

    Change:change foodId to id

---
+++ pages/menu/[id].js
@@ -0,0 +1,53 @@
+import React from 'react'
+import { useRouter } from 'next/router'
+//Components
+import DetailsPage from '../../components/templates/DetailsPage'
+
+function Details({data}) {
+    const router = useRouter()
+    if(router.isFallback){
+        return(
+            <h2>Loading page ...</h2>
+        )
+    }
+  return (
+    <DetailsPage {...data}/>
+  )
+}
+
+export default Details
+
+export async function getStaticPaths() {
+    const res = await fetch("http://localhost:4000/data")
+    const json = await res.json()
+    const data = json.slice(0,10)
+
+    const paths = data.map((food) => ({
+        params:{
+            id:food.id.toString()
+        }
+    }))
+    return{
+        paths,
+        fallback:true
+    }
+}
+export async function getStaticProps(context) {
+  console.log(context.params)
+  const {
+     params : {id},
+    } = context
+  const res = await fetch(`http://localhost:4000/data/${id}`);
+  const data = await res.json();
+  
+  if (!data.id) {
+  return {
+    notFound: true,
+      };
+  }
+      
+  return {
+    props: { data },
+    revalidate: 10,
+      };
+    }
